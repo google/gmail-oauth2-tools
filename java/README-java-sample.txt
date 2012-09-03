cf43bc0bc028a2d31b84ad8d2c318f49a2dd9f99
nicolson@google.com <nicolson@google.com@3001c1d0-8294-44a0-d982-c2f981e226c9>
2012-09-03 20:26:03 +0000

    sh script to build Java sample code ZIP.
    README file for Java sample code.

---
+++ java/README-java-sample.txt
@@ -0,0 +1,7 @@
+This sample code is distributed from http://code.google.com/p/google-mail-oauth2-tools/ under the Apache License 2.0.
+
+The Apache License 2.0 is available from
+http://www.apache.org/licenses/LICENSE-2.0.
+
+For instructions on using this sample code, see:
+http://code.google.com/p/google-mail-oauth2-tools/wiki/JavaSampleCode
---
+++ java/build-java-sample-zip.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+#
+# Copyright 2012 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Usage: build-java-sample-zip.sh [datestr]
+#
+# Copies the Java sample files to a temporary directory and zips them up.
+# The resulting ZIP file is left in /tmp.
+# If datestr is not specified, it defaults to the current date in YYYYMMDD
+# format; for example, "20120904". This would result in the zipfile
+# /tmp/oauth2-java-sample-20120904.zip.
+#
+# The script is intended to be run from the root of the Java code hierarchy.
+# It makes sure there are no local modifications to the files that are
+# being zipped up.
+
+
+files="README-java-sample.txt build.xml build.properties com/google/code/samples/oauth2/*.java"
+
+if [[ "$1" ]] ; then
+  date="$1"
+else
+  date=$(date "+%Y%m%d")
+fi
+
+relative_tmpdir="oauth2-java-sample-$date"
+full_tmpdir="/tmp/$relative_tmpdir"
+outfile="/tmp/oauth2-java-sample-$date.zip"
+
+if [[ -e $full_tmpdir ]]; then
+  echo "ERROR: directory $full_tmpdir already exists"
+  exit -1
+fi
+
+if [[ -e $outfile ]]; then
+  echo "ERROR: $outfile already exists"
+  exit -1
+fi
+
+status=$(svn status $files)
+if [[ "$status" ]] ; then
+  echo "ERROR: One or more files has uncommitted changes:"
+  echo "$status"
+  exit -1
+fi
+
+mkdir -p $full_tmpdir
+cp --parents $files $full_tmpdir
+
+cd /tmp
+zip -r $outfile $relative_tmpdir
+rm -r $full_tmpdir
+
+echo "Created $outfile"
