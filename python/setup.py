77ced24ef0c56b4890fd422d363f71a021f4ab80
tripleee <tripleee@github.com>
2016-01-28 12:36:07 +0200

    setup.py: Make this installable as a module. __init__.py: Empty file to facilitate the same.

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gmail_oauth2_tools',
+      version='0.1',
+      description='Gmail OAuth2 tools from Google',
+      url='http://github.com/google/gmail-oauth2-tools/',
+      author='Gmail',
+      author_email='google-mail-oauth2-tools@googlegroups.com',
+      license='Apache License 2.0',
+      zip_safe=False)
