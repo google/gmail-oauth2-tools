d6e6dc5824f06812ed32177da2a1815bdd689719
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-08-09 15:24:29 -0400

    added test to match Factory mechanism with Client mechanism

--- java/src/test/com/google/code/samples/oauth2/OAuth2SaslClientFactoryTests.java
+++ java/src/test/com/google/code/samples/oauth2/OAuth2SaslClientFactoryTests.java
@@ -5,10 +5,14 @@ import static org.junit.Assume.assumeTrue;
 
 import org.junit.jupiter.api.Test;
 
+import com.google.common.annotations.VisibleForTesting;
+
 import main.com.google.code.samples.oauth2.OAuth2SaslClientFactory;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import main.com.google.code.samples.oauth2.OAuth2SaslClient;
+
 public class OAuth2SaslClientFactoryTests {
 
     private final OAuth2SaslClientFactory oAuth2SaslClientFactory = new OAuth2SaslClientFactory();
@@ -29,5 +33,12 @@ public class OAuth2SaslClientFactoryTests {
     @BeforeAll
     static void initAll() {
         
+    }
+
+    @Test
+    void getMechanismNameXOAUTH()
+    {
+        OAuth2SaslClient oAuth2SaslClient = new OAuth2SaslClient();
+        assertEquals(oAuth2SaslClient.getMechanismName(), oAuth2SaslClientFactory.getMechanismNames(null)[0]);
     }
 }
