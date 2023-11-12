96090a895e055d628f23a5918a6e3479e449752f
favonia <favonia@gmail.com>
2023-11-12 04:42:54 -0600

    Use the XDG config directory by default

--- go/sendgmail/README.md
+++ go/sendgmail/README.md
@@ -25,13 +25,13 @@ send-email`.
         `https://oauth2.dance/` as an authorised redirect URI. This is necessary
         for seeing the authorisation code on a page in your browser.
 
-    *   When you download the credentials as JSON, save the file to your home
-        directory as `.sendgmail.json` with file mode `0600`.
+    *   When you download the credentials as JSON, create the directory
+        `${XDG_CONFIG_HOME:-${HOME}/.config}/sendgmail` with file mode `0700`
+        and save the file to that directory as `config.json` with file mode `0600`.
 
-        If complying with the XDG Base Directory Specification, instead create
-        the `${XDG_CONFIG_HOME:-${HOME}/.config}/sendgmail` directory with file
-        mode `0700` and then save the file to that directory as `config.json`
-        with file mode `0600`.
+        Note: the legacy configuration file at `${HOME}/.sendgmail.json` will be
+        searched if the directory `${XDG_CONFIG_HOME:-${HOME}/.config}/sendgmail`
+        does not exist.
 
 3.  Go back to **APIs & Services > OAuth consent screen** in the Google Cloud
     console.
@@ -49,12 +49,6 @@ Run the following command to build and install sendgmail to
 
 ```shell
 go install github.com/google/gmail-oauth2-tools/go/sendgmail@latest
-```
-
-If complying with the XDG Base Directory Specification, instead run:
-
-```shell
-go install -tags xdg github.com/google/gmail-oauth2-tools/go/sendgmail@latest
 ```
 
 ## Obtaining OAuth2 credentials for yourself
--- go/sendgmail/paths.go
+++ go/sendgmail/paths.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !xdg
-
 package main
 
 import (
@@ -21,6 +19,8 @@ import (
 	"os"
 	"path/filepath"
 )
+
+var useXDG bool = true
 
 func userHomeDir() string {
 	dir, err := os.UserHomeDir()
@@ -30,10 +30,50 @@ func userHomeDir() string {
 	return dir
 }
 
+func userConfigDir() string {
+	if dir := os.Getenv("XDG_CONFIG_HOME"); dir != "" {
+		return dir
+	}
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("Neither $XDG_CONFIG_HOME nor the user home directory is defined: %v.", err)
+	}
+	return filepath.Join(dir, ".config")
+}
+
+func dirExists(path string) bool {
+	st, err := os.Stat(path)
+	return err == nil && st.IsDir()
+}
+
+func fileExists(path string) bool {
+	st, err := os.Stat(path)
+	return err == nil && st.Mode().IsRegular()
+}
+
+func init() {
+	switch {
+	case dirExists(filepath.Join(userConfigDir(), "sendgmail")):
+		useXDG = true
+	case fileExists(filepath.Join(userHomeDir(), ".sendgmail.json")):
+		useXDG = false
+	default:
+		useXDG = true
+	}
+}
+
 func configPath() string {
-	return filepath.Join(userHomeDir(), ".sendgmail.json")
+	if useXDG {
+		return filepath.Join(userConfigDir(), "sendgmail", "config.json")
+	} else {
+		return filepath.Join(userHomeDir(), ".sendgmail.json")
+	}
 }
 
 func tokenPath() string {
-	return filepath.Join(userHomeDir(), ".sendgmail."+sender+".json")
+	if useXDG {
+		return filepath.Join(userConfigDir(), "sendgmail", "token."+sender+".json")
+	} else {
+		return filepath.Join(userHomeDir(), ".sendgmail."+sender+".json")
+	}
 }
